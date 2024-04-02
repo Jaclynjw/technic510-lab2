9e16ee464481ef13df4905d719c902a5b47efeea
Jaclynjw <chenjw18@uw.edu>
2024-04-02 14:30:03 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import seaborn as sns
+
+st.set_page_config(
+    page_title="Technic510 Lab 2",
+    page_icon="random",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
