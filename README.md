18ef2eeea6820fa18db0466821e8b14c21dfa4b9
Jaclynjw <chenjw18@uw.edu>
2024-04-02 15:07:13 -0700

    update

---
+++ README.md
@@ -0,0 +1,12 @@
+# Stock Data Analysis Web App
+
+## Introduction
+This web app is designed to allow users to explore and analyze stock data through an intuitive and interactive interface. Built with Streamlit and powered by `yfinance`, it provides users with the capability to select different stocks, choose a specific date range, and view the corresponding stock data visualizations and raw data.
+
+## Features
+- **Stock Selection**: Users can select from a predefined list of stock symbols (e.g., AAPL, GOOGL, MSFT, AMZN) to analyze.
+- **Date Range Filtering**: A date picker allows users to specify the start and end dates for the data they wish to view, making it easy to narrow down to specific periods of interest.
+- **Data Visualization**:
+  - **Closing Price Chart**: Displays the closing prices of the selected stock over the chosen date range.
+  - **Volume Chart**: Shows the trading volume of the selected stock over the same period.
+  - **Raw Data Table**: Provides a detailed view of the raw data fetched, including Open, High, Low, Close, and Volume.
--- app.py
+++ app.py
@@ -1,9 +1,46 @@
 import streamlit as st
 import seaborn as sns
+import yfinance as yf
+import pandas as pd
+import datetime
 
 st.set_page_config(
-    page_title="Technic510 Lab 2",
-    page_icon="random",
+    page_title="Stock Data Analysis",
+    page_icon="📈",
     layout="wide",
-    initial_sidebar_state="expanded",
+    initial_sidebar_state="auto",
 )
+
+#title
+st.title("Stock Data Analysis")
+# Introduction
+st.write("""
+This web application allows users to explore stock prices and volumes. 
+Select a stock symbol and a date range to see the closing prices and volumes.
+""")
+
+# Widget to select a stock symbol
+ticker_symbol = st.selectbox("Select a Stock Symbol", ["AAPL", "GOOGL", "MSFT", "AMZN"])
+
+# Widget to select a date range
+start_date = st.date_input("Start Date", datetime.date(2020, 1, 1))
+end_date = st.date_input("End Date", datetime.date.today())
+
+# Check if the date range is valid
+if start_date < end_date:
+    # Use yfinance to fetch the stock data
+    ticker_data = yf.Ticker(ticker_symbol)
+    ticker_df = ticker_data.history(period='1d', start=start_date, end=end_date)
+
+    # Display the closing prices and volume using line charts
+    st.write("## Closing Price")
+    st.line_chart(ticker_df.Close)
+    
+    st.write("## Volume")
+    st.line_chart(ticker_df.Volume)
+
+    st.write("## Raw Data")
+    st.dataframe(ticker_df)
+else:
+    st.error("Error: End date must be after start date.")
+
